8a848457cb42118f8285d79360c592ce89226b5e
Hanna Kostiana <anna.kostianaia@gmail.com>
2022-12-14 22:37:18 +0200

    add more

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@ import securityCheck from './js/security';
 import Notiflix from 'notiflix';
 
 const refs = {
-    passwordForm: document.querySelector('#password-form'),
+    passwordForm: document.querySelector('.password-form'),
 } 
 
 refs.passwordForm.addEventListener('submit', onCheck);
 
 function onCheck(evt) {
     evt.preventDefault();
-    const password = Number(evt.currentTarget.elements.searchQuery.value.trim());
+    const form = evt.currentTarget;
+    const password = Number(form.elements.searchQuery.value.trim());
     const result = securityCheck(password);
     if (result === true) {
         Notiflix.Notify.success('You are right!');
+        refs.passwordForm.removeEventListener('submit', onCheck);
         document.location.href = './second.html';
     } else {
         Notiflix.Notify.failure('Ho-ho-ho... Try more...');
+        refs.passwordForm.reset();
     }
-    refs.passwordForm.reset();
 }
--- src/js/security.js
+++ src/js/security.js
@@ -1,4 +1,4 @@
-import md5 from 'md5';
+
 const KEY = 48110;
 
 export default function securityCheck(password) {
